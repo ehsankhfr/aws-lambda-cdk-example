4632375d03ff42bb1c4c46000927264ea6e4fc3a
EhsanKh <ehsankhfr@gmail.com>
2022-05-27 17:05:24 +0100

    fix: cdk fix for lambda definition

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,20 +1,15 @@
 import * as cdk from 'aws-cdk-lib';
-import {NodejsFunction} from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
-
 import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as path from 'path';
 
 export class CdkStack extends cdk.Stack {
     constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        const helloWorldFunction = new NodejsFunction(this, 'helloWorld', {
-            memorySize: 1024,
-            timeout: cdk.Duration.seconds(5),
+        const helloWorldFunction = new lambda.Function(this, 'helloWorld', {
             runtime: lambda.Runtime.NODEJS_16_X,
-            handler: 'main',
-            entry: path.join(__dirname, `/../../build/app.js`),
+            code: lambda.Code.fromAsset('lambda'),
+            handler: 'hello.handler'
         });
 
         const api = new apigateway.RestApi(this, "helloWorldApi", {
