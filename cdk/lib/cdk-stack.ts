2f6e4e9a05cb245e9ed3b8a3fcf36eda648a45be
EhsanKh <ehsankhfr@gmail.com>
2022-06-01 13:13:17 +0100

    fix: dependency use example

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -6,6 +6,15 @@ export class CdkStack extends cdk.Stack {
     constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
+        // const calcLayer = new lambda.LayerVersion(this, 'calc-layer', {
+        //     compatibleRuntimes: [
+        //         lambda.Runtime.NODEJS_12_X,
+        //         lambda.Runtime.NODEJS_14_X,
+        //     ],
+        //     code: lambda.Code.fromAsset('src/layers/calc'),
+        //     description: 'multiplies a number by 2',
+        // });
+
         const helloWorldFunction = new lambda.Function(this, 'helloWorld', {
             runtime: lambda.Runtime.NODEJS_16_X,
             code: lambda.Code.fromAsset('lambda'),
