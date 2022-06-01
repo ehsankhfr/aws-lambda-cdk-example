1b46ed28a8eb4fc5d44a3da557430979981cf60a
EhsanKh <ehsankhfr@gmail.com>
2022-06-01 12:36:04 +0100

    fix: cdk changes revert

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,32 +1,22 @@
-import * as cdk from '@aws-cdk/core';
-import * as apigateway from '@aws-cdk/aws-apigatewayv2';
-import {HttpLambdaIntegration} from '@aws-cdk/aws-apigatewayv2-integrations';
-import * as lambda from '@aws-cdk/aws-lambda-nodejs';
+import * as cdk from 'aws-cdk-lib';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class CdkStack extends cdk.Stack {
     constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        const helloWorldFunction = new lambda.NodejsFunction(this, 'helloWorld', {
-            entry: '../../src/hello.ts',
-            functionName: 'hello',
-            handler: 'handler',
-            memorySize: 512,
-            timeout: cdk.Duration.seconds(10),
-            bundling: {
-                minify: true, // minify code, defaults to false
-                sourceMap: true, // include source map, defaults to false
-                sourceMapMode: lambda.SourceMapMode.INLINE, // defaults to SourceMapMode.DEFAULT
-                sourcesContent: false, // do not include original source into source map, defaults to true
-                target: 'es2020', // target environment for the generated JavaScript code
-            }
+        const helloWorldFunction = new lambda.Function(this, 'helloWorld', {
+            runtime: lambda.Runtime.NODEJS_16_X,
+            code: lambda.Code.fromAsset('lambda'),
+            handler: 'hello.handler'
         });
-        const httpApi = new apigateway.HttpApi(this, 'HttpApi');
 
-        httpApi.addRoutes({
-            path: '/',
-            methods: [apigateway.HttpMethod.GET],
-            integration: new HttpLambdaIntegration('helloWorldIntegration', helloWorldFunction)
+        const api = new apigateway.RestApi(this, "helloWorldApi", {
+            restApiName: "Hello World!",
+            description: "This service serves hello-world API."
         });
+
+        api.root.addMethod('GET', new apigateway.LambdaIntegration(helloWorldFunction, {}))
     }
 }
