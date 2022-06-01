df6deffa6ddc5b32bb8366792e3aaf7985b089e2
EhsanKh <ehsankhfr@gmail.com>
2022-06-01 12:48:01 +0100

    fix: dependency use example

--- src/hello.ts
+++ src/hello.ts
@@ -1,7 +1,12 @@
+import fetch from 'node-fetch'
+
 export const handler = async function (event: any, context: any) {
+    const response = await fetch('https://api.predic8.de/shop/products/62');
+    const res = await response.json();
+
     return {
         statusCode: 200,
         headers: {},
-        body: "Hello world!",
-    }
+        body: JSON.stringify(res)
+    };
 }
